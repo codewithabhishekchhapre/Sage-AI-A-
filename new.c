23e650eb500fdf14fb090ef3138cc16d9d034012
codewithabhishekchhapre <abhishekgurjar5020@gmail.com>
2025-05-30 11:43:23 +0530

    add new code
    add new cide

--- new.c
+++ new.c
@@ -1,3 +1,23 @@
+
 void main(){
-     
+
+     for(int i = 0; i < 6; i++) {
+
+          if(i%2==0){
+               printf("%d",i%2);
+          }
+          else{
+               printf("%d\n",i%2);
+          }
+          if(i%2==0 && i > 3){
+               printf("%d\n",i%3);
+          }
+          else if(i%2==0 || i < 5){
+               printf("%d\n",i+2);
+          }
+          else{
+               printf("%d\n",i+1);
+          }
+     }
+
 }
